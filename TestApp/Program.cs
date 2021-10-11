e97d1dded66e69488cd8850c4ec5bc576e9cc010
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-11 16:03:52 -0300

    Added working with inputs and parsing data

---
+++ TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = double.Parse(Console.ReadLine());
+
+            //imprimindo valor double com delimitação de 2 dígitos após a vírgula
+            Console.WriteLine(salario.ToString("F2"));
+        }
+    }
+}
