680d6bc639605e9d48266087a5dbd0ab0928e42d
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-11 14:22:39 -0300

    Added examples with Placeholder, Intepolation, and Concatenation in C#

---
+++ Strings-outPut-Samples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Wesley";
+            int idade = 21;
+            double salario = 2280.35232;
+
+            //placeholder
+            Console.WriteLine("{0} tem {1} anos e salario de {2:F2} reais.", nome, idade, salario);
+
+            //interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e salario de {salario:F2} reais.");
+
+            //concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos e salario de " + salario.ToString("F2") + " reais.");
+        }
+    }
+}
