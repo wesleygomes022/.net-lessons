0c33cbb6ab9ba379c4e03756fa71653d5dab383d
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-19 19:25:51 -0300

    replacing folders and adding ToString sample

---
+++ Class.Attributes.Methods.StaticMembers/ToStringSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            
+            Console.Write("Entre os dados do produto: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Insira o preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira a quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine(p);
+        }
+    }
+}
