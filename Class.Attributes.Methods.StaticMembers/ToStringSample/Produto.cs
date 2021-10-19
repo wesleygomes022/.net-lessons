3c4447f7067fd6dfed7e299ffbee1de8860af45e
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-19 19:25:51 -0300

    replacing folders and adding ToString sample

---
+++ Class.Attributes.Methods.StaticMembers/ToStringSample/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque()
+        {
+            return this.Preco * this.Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + this.Nome
+                + "\nPreço: $" + Preco
+                + "\nQuantidade: " + this.Quantidade
+                + "\nQuantidade em estoque: " + ValorTotalEstoque().ToString("F2");
+        }
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-
 namespace TestApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double salario = double.Parse(Console.ReadLine());
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
 
-            //imprimindo valor double com delimitação de 2 dígitos após a vírgula
-            Console.WriteLine(salario.ToString("F2"));
+            Console.Write("Insira o nome da 1a pessoa: ");
+            p1.setNome(Console.ReadLine());
+
+            Console.Write("Insira a idade da 1a pessoa: ");
+            p1.setIdade(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+
+            Console.Write("Insira o nome da 2a pessoa: ");
+            p2.setNome(Console.ReadLine());
+
+            Console.Write("Insira a idade da 2a pessoa: ");
+            p2.setIdade(int.Parse(Console.ReadLine()));
+
+            string pMaisVelha = Pessoa.pVelha(p1, p2);
+
+            Console.WriteLine("\nA Pessoa mais velha é: " + pMaisVelha);
+
+
         }
     }
 }
