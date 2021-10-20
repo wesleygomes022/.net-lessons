08fad19b72cb0a93dcc997a5f350ba45aafe9ca9
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-20 19:53:57 -0300

    Added Properties Sample

---
+++ Constructors.Word-This.Overloading.Encapsulation/Properties-Sample/Carro.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    class Carro
+    {
+        private string Modelo, Marca;
+        private int Ano;
+        private double Preco;
+
+        public Carro()
+        {
+            Preco = 40000.00;
+        }
+
+        public string modelo
+        {
+            get
+            {
+                return Modelo;
+            }
+
+            set
+            {
+                Modelo = value;
+            }
+        }
+
+        public string marca
+        {
+            get
+            {
+                return Marca;
+            }
+            set
+            {
+                Marca = value;
+            }
+        }
+
+         public int ano 
+        {
+            get
+            {
+                return Ano;
+            }
+
+            set
+            {
+                Ano = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Modelo + "; "
+                + Marca + "; "
+                + Ano + "; "
+                + Preco.ToString("F2");
+        }
+    }
+}
---
+++ Constructors.Word-This.Overloading.Encapsulation/Properties-Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+
+            c1.modelo = "Civic";
+            c1.ano = 2018;
+            c1.marca = "Honda";
+
+            Console.WriteLine(c1);
+        }
+    }
+}
