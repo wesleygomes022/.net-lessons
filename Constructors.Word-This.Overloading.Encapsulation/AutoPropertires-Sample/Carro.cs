9de9365100efd76f2987db8c1de67aa18303cdc3
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-20 20:08:01 -0300

    Added Auto Properties Sample

---
+++ Constructors.Word-This.Overloading.Encapsulation/AutoPropertires-Sample/Carro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    class Carro
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; private set; }
+        public int Ano { get; set; }
+        public double Preco { get; set; }
+
+        public Carro()
+        {
+            Preco = 40000.00;
+        }
+
+        public void setModelo(string modelo) 
+        {
+            Modelo = modelo;
+        }
+
+        public override string ToString()
+        {
+            return Modelo + "; "
+                + Marca + "; "
+                + Ano + "; "
+                + Preco.ToString("F2");
+        }
+    }
+}
---
+++ Constructors.Word-This.Overloading.Encapsulation/AutoPropertires-Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+
+            c1.setModelo("Civic");
+            c1.Ano = 2018;
+            c1.Marca = "Honda";
+
+            Console.WriteLine(c1);
+        }
+    }
+}
