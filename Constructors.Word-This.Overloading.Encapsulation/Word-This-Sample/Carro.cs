db0a4cbacaa8f4f2165977e1757d2c6a02df6341
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-20 19:21:13 -0300

    added sample with 'this' word

---
+++ Constructors.Word-This.Overloading.Encapsulation/Word-This-Sample/Carro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    class Carro
+    {
+        string Modelo, Marca;
+        int Ano;
+        double Preco;
+
+        public Carro(string modelo, string marca)
+        {
+            Modelo = modelo;
+            Marca = marca;
+        }
+
+        public Carro(int ano, string modelo, string marca) : this()
+        {
+            Ano = ano;
+            Modelo = modelo;
+            Marca = marca;
+        }
+
+        public Carro()
+        {
+            Preco = 40000.00;
+        }
+
+        public override string ToString()
+        {
+            return Modelo + "; "
+                + Marca + "; "
+                + Ano + "; "
+                + Preco.ToString("F2");
+        }
+    }
+}
---
+++ Constructors.Word-This.Overloading.Encapsulation/Word-This-Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro(2015, "Hb20", "Hyundai");
+            Console.WriteLine(c1);
+        }
+    }
+}
