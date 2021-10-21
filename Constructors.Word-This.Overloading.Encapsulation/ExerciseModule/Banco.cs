23c92200b9542e2963c5e599e830290803af4701
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-21 20:22:39 -0300

    Added Exercises of end of module

---
+++ Constructors.Word-This.Overloading.Encapsulation/ExerciseModule/Banco.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    class Banco
+    {
+        public int Conta { get; private set; }
+        public string TitularNome { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public Banco(double depositoInicial) 
+        {
+            Saldo = depositoInicial;
+        }
+
+        public Banco(int conta, string titular, double depositoInicial) : this(depositoInicial)
+        {
+            Conta = conta;
+            TitularNome = titular;
+        }
+
+        public void deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void saque(double valor)
+        {
+            Saldo -= valor;
+            taxaSaque();
+        }
+
+        public void taxaSaque()
+        {
+            Saldo -= 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Conta
+                + "; \nTitular: " + TitularNome
+                + "; \nSaldo: " + Saldo;
+        }
+    }
+}
---
+++ Constructors.Word-This.Overloading.Encapsulation/ExerciseModule/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco c1;
+
+            Console.Write("Insira o número da conta: ");
+            int Conta = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira o títular da conta: ");
+            string Titular = Console.ReadLine();
+
+            Console.Write("Deseja efetuar um novo depósito? (Digite true para SIM ou false para NÃO): ");
+            bool Temp = bool.Parse(Console.ReadLine());
+
+            if (Temp)
+            {
+                Console.Write("Insira o valor do depósito inicial: ");
+                double depInicial = double.Parse(Console.ReadLine());
+                c1 = new Banco(Conta, Titular, depInicial);
+            }
+            else
+            {
+                c1 = new Banco(Conta, Titular, 0.0);
+            }
+
+            Console.WriteLine("\nExtrato da conta:\n----------------------\n" + c1);
+
+            Console.Write("\nDeseja efetuar um novo depósito? (Digite true para SIM ou false para NÃO): ");
+            Temp = bool.Parse(Console.ReadLine());
+                        
+            while (Temp)
+            {
+                Console.Write("Insira o valor do depósito: ");
+                double Valor = double.Parse(Console.ReadLine());
+                c1.deposito(Valor);
+
+                Console.WriteLine("\nExtrato da conta:\n----------------------\n" + c1);
+
+                Console.Write("\nDeseja efetuar um novo depósito? (Digite true para SIM ou false para NÃO): ");
+                Temp = bool.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nDados da conta atualizados!");
+            Console.WriteLine("Extrato da conta:\n----------------------\n" + c1);
+
+            Console.Write("\nDeseja efetuar um saque? (Digite true para SIM ou false para NÃO): ");
+            Temp = bool.Parse(Console.ReadLine());
+
+            if (Temp)
+            {
+                Console.Write("Insira o valor do saque: ");
+                double Valor = double.Parse(Console.ReadLine());
+                c1.saque(Valor);
+
+                Console.WriteLine("\nDados da conta atualizados!");
+                Console.WriteLine("Extrato da conta:\n----------------------\n" + c1);
+            }
+
+            Console.Write("Deseja efetuar um novo saque? (Digite true para SIM ou false para NÃO): ");
+            Temp = bool.Parse(Console.ReadLine());
+
+            while (Temp)
+            {
+                Console.Write("Insira o valor do saque: ");
+                double Valor = double.Parse(Console.ReadLine());
+                c1.saque(Valor);
+
+                Console.WriteLine("\nExtrato da conta:\n----------------------\n" + c1);
+
+                Console.Write("Deseja efetuar um novo saque? (Digite true para SIM ou false para NÃO): ");
+                Temp = bool.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nDados da conta atualizados!");
+            Console.WriteLine("Extrato da conta:\n----------------------\n" + c1);
+        }
+    }
+}
