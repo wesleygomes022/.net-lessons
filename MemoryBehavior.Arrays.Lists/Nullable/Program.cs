85ece6b0eb7519275756c04310f72b89f9b603f3
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-29 18:34:59 -0300

    Nullable sample

---
+++ MemoryBehavior.Arrays.Lists/Nullable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<double> x = null;
+            //outra forma de escrita
+            //double? x = null;
+
+            //Console.WriteLine(x.GetValueOrDefault());
+            // retorna o valor padrão do tipo primitivo
+
+
+            //Console.WriteLine(x.HasValue);
+            // retorna true/false se a variável possui valor válido ou não
+             
+
+            Console.WriteLine(x.Value);
+            // retorna o valor da variável ou excessão se a mesma estiver com valor null
+        }
+    }
+}
