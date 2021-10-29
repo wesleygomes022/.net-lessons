f72d285c7c85a2f8db49ce69a331d331eedfa483
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-29 18:13:46 -0300

    Structs Sample

---
+++ MemoryBehavior.Arrays.Lists/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    struct Pessoa
+    {
+        public string Nome;
+        public int Idade;
+
+        public override string ToString()
+        {
+            return Nome + ", " + Idade + ".";
+        }
+    }
+}
---
+++ MemoryBehavior.Arrays.Lists/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            //p1.Nome = "Wesley";
+            //p1.Idade = 21;
+            Console.WriteLine(p1);
+        }
+    }
+}
