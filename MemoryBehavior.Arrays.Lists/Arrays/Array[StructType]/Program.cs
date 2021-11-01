ebbe699f761566d9e87217e81229560404f5a84c
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-31 22:33:12 -0300

    Added array - Struct Type sample

---
+++ MemoryBehavior.Arrays.Lists/Arrays/Array[StructType]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação array do tipo struct
+            int[] array = new int[10];
+
+            for(int i=0; i<array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Array["+i+"] = " + array[i]);
+            }
+        }
+    }
+}
