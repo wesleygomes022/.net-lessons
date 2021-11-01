e19c4beaa06281a83c9209cad3f2270504e0bf21
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-10-31 22:25:25 -0300

    Added array - Class Type sample

---
+++ MemoryBehavior.Arrays.Lists/Arrays/Array[ClassType]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação array do tipo classe
+            Produto[] p = new Produto[3];
+            for(int i=0; i<3; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine());
+                p[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double soma = 0;
+
+            for (int i=0; i<3; i++) 
+            {
+                soma += p[i].Preco;
+            }
+
+            soma /= 3;
+
+            Console.WriteLine("Preco Medio = " + soma);
+        }
+    }
+}
