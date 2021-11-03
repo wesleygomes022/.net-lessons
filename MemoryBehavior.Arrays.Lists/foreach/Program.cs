cfd042aa3e0b6fa782757ac9fdf34b1db371ed1a
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-11-03 16:20:42 -0300

    added foreach sample

---
+++ MemoryBehavior.Arrays.Lists/foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[] { "banana", "limao", "pera", "melancia", "abacaxi" };
+
+            foreach(string fruta in arr)
+            {
+                Console.WriteLine(fruta);
+            }
+        }
+    }
+}
