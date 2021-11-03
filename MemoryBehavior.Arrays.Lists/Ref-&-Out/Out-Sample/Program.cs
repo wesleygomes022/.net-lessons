87dc9fdfdebdbd9bdd4e8dfac451bd76eab70d29
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-11-03 15:45:04 -0300

    Added ref & out samples

---
+++ MemoryBehavior.Arrays.Lists/Ref-&-Out/Out-Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int res;
+            TimesThree(n, out res);
+            Console.WriteLine(res);
+        }
+
+        public static void TimesThree(int n, out int result)
+        {
+            result = n*3;
+        }
+    }
+}
