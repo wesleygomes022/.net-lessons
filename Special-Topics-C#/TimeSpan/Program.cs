3f254c5b06cc50f45ace361120682e58183b449b
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-11-16 18:46:56 -0300

    Added TimeSpan samples

---
+++ Special-Topics-C#/TimeSpan/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+ namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(0, 1, 30);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t2.Ticks);
+
+            TimeSpan t3 = new TimeSpan(900000000L);
+            Console.WriteLine(t3);
+
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+            Console.WriteLine(t4);
+
+            TimeSpan t5 = new TimeSpan(3, 5, 34, 47, 500);
+            Console.WriteLine(t5);
+
+            //instantiating TimeSpan with 'from' methods
+            TimeSpan t6 = TimeSpan.FromDays(1.5);
+            Console.WriteLine(t6);
+
+            TimeSpan t7 = TimeSpan.FromHours(1.5);
+            Console.WriteLine(t7);
+
+            TimeSpan t8 = TimeSpan.FromMinutes(1.5);
+            Console.WriteLine(t8);
+
+            TimeSpan t9 = TimeSpan.FromSeconds(1.5);
+            Console.WriteLine(t9);
+
+            TimeSpan t10 = TimeSpan.FromMilliseconds(1.5);
+            Console.WriteLine(t10);
+
+            TimeSpan t11 = TimeSpan.FromTicks(900000000L);
+            Console.WriteLine(t11);
+        }        
+    }
+}
