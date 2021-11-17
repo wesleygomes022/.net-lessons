4eba7b0f378b16a79323bde1eb268c9dbe728aae
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-11-17 11:57:25 -0300

    Added operations samples

---
+++ Special-Topics-C#/Properties-and-Operations-w-DateTime/OperationsSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+ namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2021, 11, 17, 11, 30, 45, 300);
+
+            DateTime d2 = d1.AddHours(3);
+
+            DateTime d3 = d2.AddMinutes(5);
+
+            TimeSpan d4 = d3.Subtract(d1);
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+        }        
+    }
+}
