8206be439d2c21e85c6ad0fde0b5c3d7bfa37289
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-11-17 12:18:27 -0300

    Added properties sample with TimeSpan

---
+++ Special-Topics-C#/Properties-and-Operations-w-TimeSpan/PropertiesSample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+ namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(2, 3, 5, 7, 101);
+            Console.WriteLine(t1.Days);
+            Console.WriteLine(t1.Hours);
+            Console.WriteLine(t1.Minutes);
+            Console.WriteLine(t1.Seconds);
+            Console.WriteLine(t1.Milliseconds);
+            Console.WriteLine(t1.Ticks);
+
+            Console.WriteLine("---------------");
+
+            Console.WriteLine(t1.TotalDays);
+            Console.WriteLine(t1.TotalHours);
+            Console.WriteLine(t1.TotalMinutes);
+            Console.WriteLine(t1.TotalSeconds);
+            Console.WriteLine(t1.TotalMilliseconds);
+        }        
+    }
+}
