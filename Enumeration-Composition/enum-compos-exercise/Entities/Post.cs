edb90562aaf0dab4f187568f4e4600e7b1a10813
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-12-22 17:30:17 -0300

    Added exercise

---
+++ Enumeration-Composition/enum-compos-exercise/Entities/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public Post()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            s.Append("Moment: ");
+            s.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            s.Append("Title: ");
+            s.AppendLine(Title);
+            s.Append("Content: ");
+            s.AppendLine(Content);
+            s.Append("Likes: ");
+            s.AppendLine(Likes.ToString());
+            foreach(Comment comment in Comments)
+            {
+                s.Append("Comment: ");
+                s.AppendLine(comment.Text);
+            }
+
+            return s.ToString();
+        }
+    }
+}
---
+++ Enumeration-Composition/enum-compos-exercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TestProj.Entities;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post p1 = new Post();
+            p1.Moment = DateTime.Now;
+            p1.Title = "Good night guys";
+            p1.Content = "See you tomorrow";
+            p1.Likes = 5;
+
+            Comment c1 = new Comment();
+            c1.Text = "Good night";
+
+            Comment c2 = new Comment();
+            c2.Text = "May the force be with you";
+
+            p1.Comments.Add(c1);
+            p1.Comments.Add(c2);
+
+            Post p2 = new Post(DateTime.Parse("12/21/2018 17:10:35"), 
+                "Traveling to New Zealand", 
+                "I'm going to visit this wonderful country!", 
+                37);
+
+            Comment c3 = new Comment { Text = "Have a nice trip" };
+            Comment c4 = new Comment { Text = "Wow, that's awesome!" };
+
+            p2.Comments.Add(c3);
+            p2.Comments.Add(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine("----------------");
+            Console.WriteLine(p2);
+
+
+        }      
+    }
+}
