9b556b2d72831171258b96e2800e200d70ad852f
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-12-23 18:11:31 -0300

    Added Inheritance sample

---
+++ Inheritance-Polymorphism/Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TestProj.Entities;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount ba = new BusinessAccount(8010, "Bob Brown", 100.00, 500.00);
+            Console.WriteLine(ba.Balance);
+        }
+    }
+}
