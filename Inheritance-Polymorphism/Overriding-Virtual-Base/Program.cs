afe4872dc4a0383f0757fc40508d0f57a38e607b
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-12-27 01:01:19 -0300

    Added Overriding sample

---
+++ Inheritance-Polymorphism/Overriding-Virtual-Base/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestProj.Entities;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new BusinessAccount(1229, "Wesley", 3000.00, 5000.00);
+            Account acc2 = new SavingsAccount(1320, "Luis", 3000.00, 10000.00);
+            acc1.Withdraw(1000);
+            acc2.Withdraw(1000);
+            Console.WriteLine(acc1.Balance);            
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
