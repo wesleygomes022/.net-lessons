86c885059f8af67de070a4970312cc2ad46ef679
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-12-27 23:02:53 -0300

    Added Sealed Classes and Methods

---
+++ Inheritance-Polymorphism/Sealed-Methods_and_Classes/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.00;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Inheritance-Polymorphism/Sealed-Methods_and_Classes/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj.Entities
+{
+    //cannot derive class from other sealed class
+    class SavingsAccountPlus : SavingsAccount
+    {
+        //cannot override method because it's sealed
+        public override void Withdraw(double amount)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
