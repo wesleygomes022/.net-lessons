5268ff31ed6c46b38c47bdf801c31db93b63023b
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-11 12:07:30 -0300

    Added Custom Exceptions - Part 2

---
+++ Exceptions/Custom-Exceptions-2/Entities/Reservation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TestProj.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            Checkin = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration(DateTime checkIn, DateTime checkOut)
+        {
+            TimeSpan dur = checkOut.Subtract(checkIn);
+            return (int)dur.TotalDays;
+        }
+
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            if (checkIn > checkOut)
+            {
+                return "Check-out date must be after check-in date.";
+            }
+            else if (checkIn < DateTime.Now)
+            {
+                return "Reservation dates for update must be future dates.";
+            }
+            else
+            {
+                Checkin = checkIn;
+                CheckOut = checkOut;
+
+                return null;
+            }                        
+        }
+
+        public override string ToString()
+        {
+            return "Room Number: " + RoomNumber +
+                ";\nCheck-in Date: " + Checkin.ToString("dd/MM/yyyy") +
+                ";\nCheck-out Date: " + CheckOut.ToString("dd/MM/yyyy") +
+                ";\nReservation: " + Duration(Checkin, CheckOut) + " nights;";
+        }
+    }
+}
