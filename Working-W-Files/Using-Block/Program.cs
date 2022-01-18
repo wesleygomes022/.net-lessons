f0e4c1fba84d1a7e8cf7feaa0b4142cd0df72c58
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-18 12:09:25 -0300

    Added sample of using block

---
+++ Working-W-Files/Using-Block/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileA = @"C:\temp\fileA.txt";
+            try
+            {
+                using(FileStream fs = new FileStream(fileA, FileMode.Open))
+                {
+                    using(StreamReader sr = new StreamReader(fs))
+                    {
+                        while(!sr.EndOfStream)
+                        {
+                            Console.WriteLine(sr.ReadLine());
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ops... An error occured: " + e.Message);
+            }
+        }
+    }
+}
