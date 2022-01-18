6cce84bdf7b320d30be211f6fa34a9ea6412f035
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-18 12:15:47 -0300

    Added 2nd demo of using block

---
+++ Working-W-Files/Using-Block/Demo2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileA = @"C:\temp\fileA.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(fileA))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ops... An error occured: " + e.Message);
+            }
+        }
+    }
+}
