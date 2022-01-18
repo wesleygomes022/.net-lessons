14bfbd6776fa42449158dbb034b739e708c88f32
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-18 11:34:18 -0300

    Added 2nd demo

---
+++ Working-W-Files/FileStream-StreamReader/Demo2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileA = @"C:\temp\fileA.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(fileA, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                while(!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Ops... An error occured: " + e.Message);
+            }
+            finally
+            {
+                if(fs != null)
+                    fs.Close();
+                
+                if(sr != null)
+                    sr.Close();
+            }
+            
+        }
+    }
+}
