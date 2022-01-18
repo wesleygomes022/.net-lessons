40d1c8d50377c7516b3847cd6998cdceb73f27db
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-17 23:00:09 -0300

    Added sample of working with File, FileInfo and IOException

---
+++ Working-W-Files/File-FileInfo-IOException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\fileA.txt";
+            string targetPath = @"C:\temp\fileB.txt";
+            
+            try
+            {                
+                FileInfo fi = new FileInfo(sourcePath);
+                //fi.CopyTo(targetPath);
+                File.Copy(sourcePath, targetPath);
+                string[] lines = File.ReadAllLines(targetPath);
+
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured: " + e.Message);
+            }            
+        }
+    }
+}
