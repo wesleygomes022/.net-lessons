b030501470f10ba1a21fbefd8669581bac5e52a6
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-18 11:33:09 -0300

    Updated addind finally block

--- Working-W-Files/FileStream-StreamReader/Program.cs
+++ Working-W-Files/FileStream-StreamReader/Program.cs
@@ -9,28 +9,37 @@ namespace TestProj
         static void Main(string[] args)
         {
             string fileA = @"C:\temp\fileA.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            List<string> list = new List<string>();
 
             try
             {
-                FileStream fs = new FileStream(fileA, FileMode.Open);
-                StreamReader sr = new StreamReader(fs);
-                List<string> list = new List<string>();
-                while(!sr.EndOfStream)
+                fs = new FileStream(fileA, FileMode.Open);
+                sr = new StreamReader(fs);                
+                while (!sr.EndOfStream)
                 {
                     list.Add(sr.ReadLine());
                 }
-                
-                foreach(string el in list)
+
+                foreach (string el in list)
                 {
                     Console.WriteLine(el);
                 }
 
             }
-            catch(IOException e)
+            catch (IOException e)
             {
                 Console.WriteLine("Ops... An error occured: " + e.Message);
             }
-            
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+
+                if (sr != null)
+                    sr.Close();
+            }
         }
     }
 }
