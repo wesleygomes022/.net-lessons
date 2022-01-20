562717ca3e014556dbf4c645987c8c7d6ef778b8
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-20 14:20:05 -0300

    Added sample with Class Path

---
+++ Working-W-Files/Path/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\Notes\Note1.txt";
+
+            Console.WriteLine("Directory name: " + Path.GetDirectoryName(path));
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetFileName(path));
+            Console.WriteLine("File name without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Full path: " + Path.GetFullPath(path));
+            Console.WriteLine("Temp path:  " + Path.GetTempPath());
+            Console.WriteLine("Directory Separator: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("Path separator: " + Path.PathSeparator);
+        }
+    }
+}
