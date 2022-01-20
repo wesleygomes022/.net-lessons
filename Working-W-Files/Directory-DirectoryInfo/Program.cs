f79326ab51c895e94868da5c3ce332e446e91548
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-20 13:41:33 -0300

    Added sample of Directory and DirectoryInfo

---
+++ Working-W-Files/Directory-DirectoryInfo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp";
+
+            try
+            {
+                IEnumerable<string> temp = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                foreach(string el in temp)
+                {
+                    Console.WriteLine(el);
+                }
+
+                Console.WriteLine();
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach(string el in files)
+                {
+                    Console.WriteLine(el);
+                }
+
+                DirectoryInfo di = Directory.CreateDirectory(path + @"\Wesley");
+                Console.WriteLine();
+                Console.WriteLine(di);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            
+        }
+    }
+}
