4cf8f1915bd985d4258b1e6924f3bc43734bbace
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-01-25 14:51:09 -0300

    Added final exercise of the chapter

---
+++ Working-W-Files/Exercise/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+
+        public Product()
+        { }
+
+        public Product(string name, double price, double qtd)
+        {
+            Name = name;
+            Price = price;
+            Quantity = qtd;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Working-W-Files/Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using TestProj.Entities;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\temp\Products.txt";
+            string targetPath = @"C:\temp\out";
+            string targetFile = targetPath + @"\summary.csv";
+            string[] arr = File.ReadAllLines(filePath);
+            Directory.CreateDirectory(targetPath);
+            try
+            {
+                using(StreamWriter rs = File.AppendText(targetFile))
+                {
+                    foreach(string el in arr)
+                    {
+                        string[] arr2 = el.Split(",");
+                        Product p = new Product(arr2[0], double.Parse(arr2[1]), double.Parse(arr2[2]));
+                        rs.WriteLine($"{p.Name},{p.TotalPrice()}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
