d2fc027ea076a6f0530862ad456a285ddfb5af92
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-03-04 11:19:07 -0300

    Added Sample Implementation Without using Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using TestProj.Entities;
+using TestProj.Services;
+
+namespace TestProj
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data:");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (MM/dd/yyyy hh:mm): ");
+            DateTime pickup = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (MM/dd/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(pickup, finish, new Vehicle(model));
+            RentalService rs = new RentalService();
+            rs.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
+
