c0b81356a318d7c26bd4b6dca5baea55eeeb5bab
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-04 15:03:28 -0300

    Added IComparable usage example

---
+++ Interfaces/IComparable/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string info)
+        {
+            string[] temp = info.Split(',');
+            Name = temp[0];
+            Salary = double.Parse(temp[1]);
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name
+                + "; Salary: " + Salary;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Employee temp = obj as Employee;
+            return Name.CompareTo(temp.Name);
+            //return Salary.CompareTo(temp.Salary);
+        }
+    }
+}
---
+++ Interfaces/IComparable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\names.txt";
+            Employee[] arr = new Employee[9];
+            using(StreamReader sr = File.OpenText(file))
+            {
+                int acc = 0;
+                while(!sr.EndOfStream)
+                {
+                    arr[acc] = new Employee(sr.ReadLine());
+                    acc++;
+                }
+            }
+
+            Array.Sort(arr);
+
+            foreach(Employee el in arr)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
