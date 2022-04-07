8acb947cfaf56e5253fd0fcb6fe333f3e4da97a7
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-07 14:27:01 -0300

    Added first sample of Generics

---
+++ Generics-Set-Dictionary/Generics-Example_I/PrintService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//using System.Collections.Generic;
+
+namespace TestProj
+{
+    class PrintService<T>
+    {
+        private T[] Values { get; set; }
+        private int Count { get; set; } = 0;
+
+        public PrintService(int size)
+        {
+            Values = new T[size];
+        }
+
+        public void AddValue(T num)
+        {
+            if (Count == Values.Length)
+            {
+                throw new Exception("Buffer is full.");
+            }                
+            else
+            {
+                Values[Count] = num;
+                Count++;
+            }
+        }
+
+        public T First()
+        {
+            if (Count == 0)
+                throw new Exception("Buffer is empty.");
+            else
+                return Values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for(int i = 0; i < Values.Length-1; i++)
+            {
+                Console.Write($"{Values[i]}, ");
+            }
+            Console.Write($"{Values[Values.Length-1]}]");
+        }
+    }
+}
---
+++ Generics-Set-Dictionary/Generics-Example_I/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many values? ");
+            int size = int.Parse(Console.ReadLine());
+
+            PrintService<string> ps = new PrintService<string>(3);
+            for(int i=0; i<size; i++)
+            {
+                string temp = Console.ReadLine();
+                ps.AddValue(temp);
+            }
+
+            int x = int.Parse(ps.First());
+
+            ps.Print();
+            Console.WriteLine("\nFirst: " + ps.First());
+
+        }
+    }
+}
