ec52f7a5db76ef149993bd2e9bb85fadd5df20d2
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-08 18:29:50 -0300

    Added sample: how Hash collections do comparisons

---
+++ Generics-Set-Dictionary/Hash-Comparison/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}";
+        }
+
+        /*public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Product temp = obj as Product;
+            return Name.Equals(temp.Name) && Price.Equals(temp.Price);
+        }*/
+    }
+}
---
+++ Generics-Set-Dictionary/Hash-Comparison/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using TestProj.Entities;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
+
+            Product prod = new Product("Notebook", 1200.00);
+            
+            //this first validation should return false, since the class Product don't 
+            //implement GetHashCode and Equals methods. Therefore, the method Contains 
+            //will validate both objects using memory referencing.
+            //After implement those methods said above, the method Contains may make the 
+            //validation using value comparison, then the result will be true.
+            //You can test this behavior by commenting and discommenting the Equals and 
+            //GetHashCode methods implementation in the Product class and checking the 
+            //result.
+            Console.WriteLine(a.Contains(prod));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Point point = new Point(3, 4);
+
+            //this time, the return will be true. When the data type is a struct,
+            //the Contains function makes the comparison using through the value, 
+            //not the reference. Therefore, it's not necessary implement GetHashCode
+            //and Equals in the struct class. 
+            Console.WriteLine(b.Contains(point));
+
+        }       
+    }
+}
