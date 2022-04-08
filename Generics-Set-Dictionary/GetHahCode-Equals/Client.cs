7cee5e064ecf7df7b367a374f2aa588876bf93a4
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-08 14:41:01 -0300

    Added sample of GetHashCode and Equals methods

---
+++ Generics-Set-Dictionary/GetHahCode-Equals/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Client temp = obj as Client;
+            return Name.Equals(temp.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
---
+++ Generics-Set-Dictionary/GetHahCode-Equals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
+            Client b = new Client { Name = "Maria", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode() == b.GetHashCode());
+        }
+    }
+}
