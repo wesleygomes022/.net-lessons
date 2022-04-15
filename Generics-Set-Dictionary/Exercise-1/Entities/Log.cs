19711839ecc089344375cbccd68300af3a649b0e
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-14 21:16:11 -0300

    Added Exercise 1

---
+++ Generics-Set-Dictionary/Exercise-1/Entities/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestProj.Entities
+{
+    class Log
+    {
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+        public Log(string name, string time)
+        {
+            Name = name;
+            Time = time;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Log temp = obj as Log;
+            return Name.Equals(temp.Name);
+        }
+    }
+}
---
+++ Generics-Set-Dictionary/Exercise-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using TestProj.Entities;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\temp\logs.txt";
+            HashSet<Log> hashTable = new HashSet<Log>();
+            using (StreamReader sr = File.OpenText(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] temp = sr.ReadLine().Split(' ');
+                    Log tempLog = new Log(temp[0], temp[1]);
+                    hashTable.Add(tempLog);
+                }
+            }
+
+            Console.WriteLine(hashTable.Count);
+        }
+    }
+}
