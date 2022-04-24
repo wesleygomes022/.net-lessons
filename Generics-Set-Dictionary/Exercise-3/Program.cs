b15cca7193c20c8412ff30f56fdc953d62e2e271
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-23 21:06:42 -0300

    Added Exercise 3

---
+++ Generics-Set-Dictionary/Exercise-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Studies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\temp\logs.txt";
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            using(StreamReader sr = File.OpenText(file))
+            {
+                while(!(sr.EndOfStream))
+                {
+                    string[] temp = sr.ReadLine().Split(',');
+                    string candidate = temp[0];
+                    int votes = int.Parse(temp[1]);
+                    if (dic.ContainsKey(candidate))
+                    {
+                        dic[candidate] += votes;
+                    }
+                    else
+                    {
+                        dic[candidate] = votes;
+                    }
+                }
+            }
+
+            foreach(KeyValuePair<string, int> el in dic)
+            {
+                Console.WriteLine(el.Key + ": " + el.Value);
+            }
+        }
+    }
+}
