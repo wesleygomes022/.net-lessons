375974114caf68f6c99640667e922e1821f8e90b
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-27 09:21:30 -0300

    Added example 2

---
+++ Extension-methods/Example-2/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace System
+{
+    static class DateTimeExtensions
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if(thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+
+            return thisObj.Substring(0, count) + "...";
+        }
+    }
+}
---
+++ Extension-methods/Example-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Studies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
