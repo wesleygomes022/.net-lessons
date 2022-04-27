237af4e9c3277e666819b4f2882b377a5654144e
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-04-27 09:12:48 -0300

    Added example 1

---
+++ Extension-methods/Example-1/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace System
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime dt)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(dt);
+            if(duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1") + " hours.";
+            }
+            return duration.Days.ToString("F1") + " days."; 
+        }
+    }
+}
---
+++ Extension-methods/Example-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Studies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 4, 25, 10, 01, 12);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
