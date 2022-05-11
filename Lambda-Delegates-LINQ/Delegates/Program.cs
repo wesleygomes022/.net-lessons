649e54017674b879819658c6e28c4eab867d7a01
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-05-11 14:52:42 -0300

    Added sample of delegate

---
+++ Lambda-Delegates-LINQ/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Studies.Services;
+
+namespace Studies
+{
+    delegate double NumericOperation(double a1, double a2);
+    delegate double SquareOperation(double n);
+    delegate double SumOperation(double a1, double a2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumericOperation operation = CalculationService.Max;
+            SquareOperation op = CalculationService.Square;
+            SumOperation sumOp = CalculationService.Soma;
+
+            double result = operation(13, 17);
+            double res = op(9);
+            double resul = sumOp(14, 26);
+
+            Console.WriteLine(result);
+            Console.WriteLine(res);
+            Console.WriteLine(resul);
+        }
+    }
+}
---
+++ Lambda-Delegates-LINQ/Delegates/Services/CalculationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Studies.Services
+{
+    class CalculationService
+    {
+        public static double Soma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Max(double a, double b)
+        {
+            return a > b ? a : b;
+        }
+
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
