db34dd19d954b804380cdbd99e8c33d65c6c350f
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-05-13 16:42:10 -0300

    Added Multicast delegates example

---
+++ Lambda-Delegates-LINQ/Multicast_Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Studies.Services;
+
+namespace Studies
+{
+    delegate void Operation(double x, double y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //referencing more the one function with a delegate. 
+            //it calls Multicast Delegate
+            Operation op = CalculationService.ShowMax;
+            op += CalculationService.ShowSum;
+
+            //invoking all functions in other of declaration
+            //op.Invoke(10, 20);
+            op(20, 25);
+        }
+    }
+}
---
+++ Lambda-Delegates-LINQ/Multicast_Delegates/Services/CalculationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Studies.Services
+{
+    class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double res = x + y;
+            Console.WriteLine(res.ToString("F2"));
+        }
+    }
+}
