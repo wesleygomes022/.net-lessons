87e3231530bed9f04760d0c4a4d4ecf4b0262b38
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-05-24 18:36:48 -0300

    Added example of introduction to Linq

---
+++ Lambda-Delegates-LINQ/Linq-Introduction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Studies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 5, 6, 7, 8 };
+
+            List<int> result = arr.Where((n) => n % 2 == 0).Select((n) => n * 10).ToList();
+
+            foreach(int el in result)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
