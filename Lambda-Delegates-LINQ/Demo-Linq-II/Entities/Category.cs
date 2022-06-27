a8cccd77d48d48dc3024ea381232b7a6152bdef0
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2022-06-27 19:43:23 -0300

    Added second demo of Linq operations

---
+++ Lambda-Delegates-LINQ/Demo-Linq-II/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Studies.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+        public Category()
+        {
+
+        }
+    }
+}
---
+++ Lambda-Delegates-LINQ/Demo-Linq-II/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Studies.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Category Category { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Id + ", "
+                + Name + ", "
+                + Price.ToString("F2") + ", "
+                + Category.Name + ", "
+                + Category.Tier;
+        }
+    }
+}
